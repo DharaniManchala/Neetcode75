5d9e205607714c315a2d0da705681083494e24ee
Dharani-2005804 <dharanimanchala48@gmail.com>
2025-08-24 23:24:17 +0530

    implement container with most water

--- Twopointers/threesum.py
+++ Twopointers/threesum.py
@@ -1,35 +1,59 @@
-def threeSum(nums):
-    nums.sort()   # Step 1: sort the array
-    result = []
-
+def threenums(nums):
+    nums.sort()
+    result=[]
     for i in range(len(nums)):
-        # Skip duplicate fixed numbers
-        if i > 0 and nums[i] == nums[i-1]:
+        if i>0 and nums[i]==nums[i-1]:
             continue
+        left,right=i+1,len(nums)-1
 
-        left, right = i+1, len(nums)-1
-
-        while left < right:
-            total = nums[i] + nums[left] + nums[right]
-
-            if total < 0:
-                left += 1
-            elif total > 0:
-                right -= 1
+        while left<right:
+            total=nums[i]+nums[left]+nums[right]
+            if total<0:
+                left+=1
+            elif total>0:
+                right-=1
             else:
-                result.append([nums[i], nums[left], nums[right]])
+                result.append([nums[i],nums[left],nums[right]])
+                while left<right and nums[left]==nums[left+1]:
+                    left+=1
+                while left<right and nums[right]==nums[right-1]:
+                    right-=1
+                left+=1
+                right-=1
+# def threeSum(nums):
+#     nums.sort()   # Step 1: sort the array
+#     result = []
 
-                # Skip duplicates for left and right
-                while left < right and nums[left] == nums[left+1]:
-                    left += 1
-                while left < right and nums[right] == nums[right-1]:
-                    right -= 1
+#     for i in range(len(nums)):
+#         # Skip duplicate fixed numbers
+#         if i > 0 and nums[i] == nums[i-1]:
+#             continue
 
-                left += 1
-                right -= 1
+#         left, right = i+1, len(nums)-1
 
-    return result
+#         while left < right:
+#             total = nums[i] + nums[left] + nums[right]
+
+#             if total < 0:
+#                 left += 1
+#             elif total > 0:
+#                 right -= 1
+#             else:
+#                 result.append([nums[i], nums[left], nums[right]])
+
+#                 # Skip duplicates for left and right
+#                 while left < right and nums[left] == nums[left+1]:
+#                     left += 1
+#                 while left < right and nums[right] == nums[right-1]:
+#                     right -= 1
+
+#                 left += 1
+#                 right -= 1
+
+#     return result
 
 
-# Example usage
-print(threeSum([-1, 0, 1, 2, -1, -4]))  # Output: [[-1, -1, 2], [-1, 0, 1]]
+# # Example usage
+# print(threeSum([-1, 0, 1, 2, -1, -4]))  # Output: [[-1, -1, 2], [-1, 0, 1]]
+# time complexity O(n^2)
+# space complexity O(n)
